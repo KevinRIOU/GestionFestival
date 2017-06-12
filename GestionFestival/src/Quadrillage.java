93ec0c1c9db9f5200f3ee6835d515f235e7c0ce8
Kévin RIOU <riou.kkevin@gmail.com>
2017-06-12 17:46:11 +0200

    fzefe
    fef

--- GestionFestival/src/Quadrillage.java
+++ GestionFestival/src/Quadrillage.java
@@ -1,6 +1,44 @@
 import java.util.ArrayList;
 
 public class Quadrillage {
-	private ArrayList<Case> quadrillage;
-	erhyvgyuehuezhcuidbcvyhdziuchuezhicez
+	private Case[][] quadrillage;
+	private int taillex,tailley;
+	
+	public Quadrillage(int x, int y) {
+		taillex=x;
+		tailley=y;
+		quadrillage = new Case[x][y];
+		for(int a = 0;a<x;a++){
+			for(int b = 0;b<y;b++){
+				quadrillage[a][b] = new Case(a,b,"void",false);
+			}
+		}
+	}
+	
+	public Case found(int x, int y){
+		x=x-1;
+		y=y-1;
+		Case result = quadrillage[x][y];
+		return result;
+	}
+	
+	public void setOccupe(int x, int y){
+		this.found(x, y).setOccupe(true);
+	}
+	public void setNotOccupe(int x, int y){
+		this.found(x, y).setOccupe(false);
+	}
+	
+	public void afficheConsole(){
+		for(int a = 0;a<taillex;a++){
+			for(int b = 0;b<tailley;b++){
+				if(quadrillage[a][b].isOccupe()){
+					System.out.print("x ");
+				}else {
+					System.out.print("o ");
+				}
+			}
+			System.out.println("");
+		}
+	}
 }
---
+++ GestionFestival/src/TempMainquadrillage.java
@@ -0,0 +1,11 @@
+
+public class TempMainquadrillage {
+
+	public static void main(String[] args) {
+		Quadrillage q = new Quadrillage(10,4);//hauteur,largeur;
+		q.setOccupe(2, 3);
+		q.setOccupe(2, 3);
+		q.afficheConsole();
+	}
+
+}
