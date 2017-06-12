cc41609b9fec3956bce8085d6eb86db9bb79fa7f
Riou Kévin <riou.kevjeux@gmail.com>
2017-06-12 20:39:48 +0200

    Update Test Eclipse

--- GestionFestival/src/TempMainquadrillage.java
+++ GestionFestival/src/TempMainquadrillage.java
@@ -4,7 +4,7 @@ public class TempMainquadrillage {
 	public static void main(String[] args) {
 		Quadrillage q = new Quadrillage(10,4);//hauteur,largeur;
 		q.setOccupe(2, 3);
-		q.setOccupe(2, 3);
+		q.setOccupe(3, 3);
 		q.afficheConsole();
 	}
 
